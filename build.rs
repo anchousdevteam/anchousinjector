61ca65182895a6737efa69438f94db8d02437ace
anchousdevteam <tewiqtemple@gmail.com>
2026-05-08 21:03:15 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,1 +1,12 @@
+fn main() {
+    cc::Build::new()
+        .cpp(true)
+        .file("src/injector.cpp")
+        .define("_CRT_SECURE_NO_WARNINGS", None) // Убираем ворнинги MSVC
+        .compile("injector_logic");
 
+    println!("cargo:rustc-link-lib=user32");
+    println!("cargo:rustc-link-lib=advapi32");
+    println!("cargo:rustc-link-lib=shell32");
+    println!("cargo:rustc-link-lib=ole32");
+}
